4386419df13ba89d649991689d32f98253c7d2b0
Seungwoo Han <swoohan@kakao.com>
2022-08-03 18:51:07 +0900

    Create SinLU.py

---
+++ SinLU.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+from tensorflow.keras import layers
+
+
+class SinLU(layers.Layer):
+    def __init__(self):
+        super(SinLU, self).__init__()
+        self.a = tf.Variable(name="a", initial_value=1.0, trainable=True)
+        self.b = tf.Variable(name="b", initial_value=1.0, trainable=True)
+
+    def call(self, x):
+        return (x+self.a*tf.sin(self.b*x))*tf.sigmoid(x)
+
+
+layer = SinLU()
+
+print(layer(tf.random.uniform([1, 10, 1])))
